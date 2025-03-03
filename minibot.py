c5a13b1cb506e92c3bb7e90950bdda690e244aa2
Narimre <55599660+Narimre@users.noreply.github.com>
2025-03-03 15:02:21 +0100

    Update minibot.py

--- minibot.py
+++ minibot.py
@@ -1,9 +1,7 @@
 import ollama
 import gradio as gr
-
 def chat(user_input,history):
     response = ollama.chat(model="phi3", messages=[{"role": "user", "content": user_input}])
     return response["message"]["content"]
-
 demo = gr.ChatInterface(fn=chat, type="messages", title="Ollama Chatbot")
 demo.launch()
